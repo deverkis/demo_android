00448dae6776265fa397b28f9ba5ae8cc62d0d9a
fengjianming <fjm20016@qq.com>
2024-07-25 13:39:10 +0800

    状态提升。

--- codelabs1/app/src/main/java/cn/kt66/codelabs1/WaterCounter.kt
+++ codelabs1/app/src/main/java/cn/kt66/codelabs1/WaterCounter.kt
@@ -7,15 +7,43 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import cn.kt66.codelabs1.WellnessTaskItem
 
 @Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column {
+        if (count > 0) {
+            Text(text = "You've had $count glasses.", modifier = modifier.padding(16.dp))
+        }
+        Button(onClick = onIncrement, enabled = count < 10) {
+            Text("Add one")
+        }
+    }
+}
+
+/**
+ * 状态提升
+ */
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var waterCount  by rememberSaveable {
+        mutableStateOf(0)
+    }
+/*    var juiceCount   by rememberSaveable {
+        mutableStateOf(0)
+    }*/
+    StatelessCounter(count = waterCount, { waterCount++ }, modifier)
+    //StatelessCounter(count = juiceCount, { juiceCount++ }, modifier)
+}
+
+@Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
+        var count by rememberSaveable { mutableStateOf(0) }
         if (count > 0) {
             var showTask by remember { mutableStateOf(true) }
             if (showTask) {
--- codelabs1/app/src/main/java/cn/kt66/codelabs1/WellnessScreen.kt
+++ codelabs1/app/src/main/java/cn/kt66/codelabs1/WellnessScreen.kt
@@ -1,10 +1,12 @@
 package cn.kt66.codelabs1
 
+import StatefulCounter
 import WaterCounter
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun WellnessScreen(modifier:Modifier=Modifier){
-    WaterCounter(modifier)
+    //WaterCounter(modifier)
+    StatefulCounter(modifier)
 }
