8ec3ff30d86245ec00937a319fa60af5e3cd7c9e
fengjianming <fjm20016@qq.com>
2024-08-12 12:27:49 +0800

    async await 用法

--- codelabs1/app/src/main/java/cn/kt66/codelabs1/MainActivity.kt
+++ codelabs1/app/src/main/java/cn/kt66/codelabs1/MainActivity.kt
@@ -1,7 +1,6 @@
 package cn.kt66.codelabs1
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,7 +11,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import cn.kt66.codelabs1.ui.theme.Codelabs1Theme
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlin.system.measureTimeMillis
 
@@ -47,22 +49,32 @@ fun MyApp() {
     Text("Myapp")
     val time = measureTimeMillis {
         runBlocking {
-            printForecast()
-            printTemperature()
+            // 异步代码
+            println("Weather forecast")
+            val forecast: Deferred<String> = async {
+                getForecast()
+            }
+            val temperature: Deferred<String> = async {
+                getTemperature()
+            }
+            //输出每个协程的值
+            println("${forecast.await()} ${temperature.await()}")
+            println("Have a good day!")
         }
     }
-    println("Execution time: ${time /1000.0} seconds")
+    println("Execution time: ${time / 1000.0} seconds")
 
 }
 
-suspend fun printForecast(){
+suspend fun getForecast(): String {
     delay(1000)
-    println("Sunny")
+    //println("Sunny")
+    return "Sunny"
 }
 
-suspend fun printTemperature(){
+suspend fun getTemperature(): String {
     delay(1000)
-    println("30\u00b0C")
+    return "30\u00b0C"
 }
 
 @Preview(showBackground = true)
