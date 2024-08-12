54478ef73a2b8d1cf72c664a2495562b6dd6edd1
fengjianming <fjm20016@qq.com>
2024-08-12 13:48:33 +0800

    模拟两位选手赛跑

--- codelabs1/app/src/main/java/cn/kt66/codelabs1/MainActivity.kt
+++ codelabs1/app/src/main/java/cn/kt66/codelabs1/MainActivity.kt
@@ -10,12 +10,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import cn.kt66.codelabs1.ui.RaceTrackerApp
 import cn.kt66.codelabs1.ui.theme.Codelabs1Theme
 import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import kotlin.system.measureTimeMillis
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +33,8 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     //WellnessScreen()
-                    MyApp()
+                    //MyApp()
+                    RaceTrackerApp()
                 }
             }
         }
@@ -51,29 +56,53 @@ fun MyApp() {
         runBlocking {
             // 异步代码
             println("Weather forecast")
-            val forecast: Deferred<String> = async {
-                getForecast()
-            }
-            val temperature: Deferred<String> = async {
-                getTemperature()
-            }
-            //输出每个协程的值
-            println("${forecast.await()} ${temperature.await()}")
+            println(getWeatherReport())
             println("Have a good day!")
         }
     }
     println("Execution time: ${time / 1000.0} seconds")
 
+    runBlocking {
+        println("${Thread.currentThread().name} - runBlocking function")
+        launch {
+            println("${Thread.currentThread().name} - launch function")
+            withContext(Dispatchers.Default) {
+                println("${Thread.currentThread().name} - withContext function")
+                delay(1000)
+                println("10 results found.")
+            }
+            println("${Thread.currentThread().name} - end of launch function")
+        }
+        println("Loading...")
+    }
+
+}
+
+/**
+ * coroutineScope{} 用于为此天气预报任务创建局部作用域
+ * coroutineScope 只有在所有工作完成后才会返回。
+ */
+suspend fun getWeatherReport() = coroutineScope {
+    //launch() 和 async() 是 CoroutineScope 的扩展函数。
+    val forecast = async { getForecast() }
+    val temperature = async{
+            getTemperature()
+    }
+    // 天气预报只包含天气预报信息 Sunny，但不包含温度，因为相应协程已取消。
+    temperature.cancel()
+    "${forecast.await()}"
+
+   // "${forecast.await()} ${temperature.await()}"
 }
 
 suspend fun getForecast(): String {
     delay(1000)
-    //println("Sunny")
     return "Sunny"
 }
 
 suspend fun getTemperature(): String {
-    delay(1000)
+    delay(500)
+    throw AssertionError("Temperature is invalid")
     return "30\u00b0C"
 }
 
@@ -82,6 +111,6 @@ suspend fun getTemperature(): String {
 fun GreetingPreview() {
     Codelabs1Theme {
         //WellnessScreen()
-        MyApp()
+        RaceTrackerApp()
     }
 }
