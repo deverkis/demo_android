6a7cea2ae6c82b1927bdc37de4e33bc5f658a465
fengjianming <fjm20016@qq.com>
2024-08-20 14:13:58 +0800

    Unit2Test3

--- codelabs1/app/src/main/java/cn/kt66/codelabs1/MainActivity.kt
+++ codelabs1/app/src/main/java/cn/kt66/codelabs1/MainActivity.kt
@@ -1,17 +1,26 @@
 package cn.kt66.codelabs1
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,9 +30,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import cn.kt66.codelabs1.ui.MarsPhotosApp
 import cn.kt66.codelabs1.ui.theme.Codelabs1Theme
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
@@ -32,6 +41,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import java.text.NumberFormat
 import kotlin.system.measureTimeMillis
 
 class MainActivity : ComponentActivity() {
@@ -48,7 +58,8 @@ class MainActivity : ComponentActivity() {
                     //MyApp()
                     //RaceTrackerApp()
                     //MarsPhotosApp()
-                    unit2Test2()
+                    // unit2Test2()
+                    UnitTest3()
                 }
             }
         }
@@ -127,20 +138,93 @@ fun unit2Test2() {
     //var result = 1
     var result by remember { mutableStateOf(1) }
     Column(modifier = Modifier, horizontalAlignment = Alignment.CenterHorizontally) {
-        val imageResource = when(result){
-            1->R.drawable.dice_1
-            2->R.drawable.dice_2
-            3->R.drawable.dice_3
-            4->R.drawable.dice_4
-            5->R.drawable.dice_5
+        val imageResource = when (result) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        Image(painter = painterResource(id = imageResource), contentDescription = imageResource.toString())
+        Image(
+            painter = painterResource(id = imageResource),
+            contentDescription = imageResource.toString()
+        )
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { result = (1..6).random()}) {
+        Button(onClick = { result = (1..6).random() }) {
             Text(stringResource(id = R.string.roll))
         }
     }
+
+}
+
+@Composable
+fun UnitTest3() {
+    Column(
+        modifier = Modifier
+            .statusBarsPadding()
+            .padding(horizontal = 40.dp)
+            .safeDrawingPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(R.string.calculate_tip),
+            modifier = Modifier
+                .padding(bottom = 16.dp, top = 40.dp)
+                .align(alignment = Alignment.Start)
+        )
+        EditNumberField(
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
+        )
+        Text(
+            text = stringResource(R.string.tip_amount, "$0.00"),
+            style = MaterialTheme.typography.displaySmall
+        )
+        Spacer(modifier = Modifier.height(150.dp))
+    }
+}
+
+@SuppressLint("UnrememberedMutableState")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditNumberField(modifier: Modifier = Modifier) {
+    //val amountInput="0"
+    //您需要依赖名为“重组”的进程来更新应用的组合。
+    //而 MutableState 类型是可变的。可以使用 mutableStateOf() 函数来创建可观察的 MutableState。
+    //var amountInput:MutableState<String> =  mutableStateOf("0")
+    //可组合函数可以使用 remember 跨重组存储对象。
+    var amountInput by remember { mutableStateOf("") }
+    val amount = amountInput.toDoubleOrNull() ?: 0.0
+    val tip = calculateTip(amount)
+    //在 lambda 表达式中，it 变量包含新值。
+    //singleLine 这样可以将文本框从多行压缩成可水平滚动的单行。
+    TextField(
+        value = amountInput,
+        onValueChange = { amountInput = it },
+        label = { Text(stringResource(id = R.string.bill_amount)) },
+        modifier = modifier,
+        singleLine = true,
+        //将键盘类型设置为数字键盘即可输入数字
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    )
+}
+
+@Composable
+fun TipTimeLayout() {
+    Column {
+        Text(
+            text = stringResource(id = R.string.tip_amount, "$0.00"),
+            style = MaterialTheme.typography.displaySmall
+        )
+    }
+}
+
+private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
+    val tip = tipPercent / 100 * amount
+    return NumberFormat.getCurrencyInstance().format(tip)
 }
 
 @Preview(showBackground = true)
@@ -150,6 +234,6 @@ fun GreetingPreview() {
         //WellnessScreen()
         //RaceTrackerApp()
         //MarsPhotosApp()
-        unit2Test2()
+        UnitTest3()
     }
 }
