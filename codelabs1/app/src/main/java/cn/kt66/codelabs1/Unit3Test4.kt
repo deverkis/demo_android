6a2093a49aa0e519a8c8f15be2d34d03e1e374d9
fengjianming <fjm20016@qq.com>
2024-08-22 19:57:38 +0800

    groupBy sortedBy 用法

--- codelabs1/app/src/main/java/cn/kt66/codelabs1/Unit3Test4.kt
+++ codelabs1/app/src/main/java/cn/kt66/codelabs1/Unit3Test4.kt
@@ -3,9 +3,9 @@ import androidx.compose.runtime.Composable
 
 class Cookie(
     val name: String,
-    val softBaked:Boolean,
-    val hasFilling:Boolean,
-    val price:Double
+    val softBaked: Boolean,
+    val hasFilling: Boolean,
+    val price: Double
 )
 
 val cookies = listOf(
@@ -54,13 +54,14 @@ val cookies = listOf(
 )
 
 @Composable
-fun Unit3Test4(){
+fun Unit3Test4() {
     Text("Unit3 Test 4")
     test1()
     test2()
     test3()
 }
-fun test1(){
+
+fun test1() {
     //使用 forEach() 循环遍历列表
     //forEach(action: (T) -> Unit)
     cookies.forEach {
@@ -70,9 +71,9 @@ fun test1(){
     //如需访问属性并将其嵌入字符串，您需要一个表达式。您可以用大括号将表达式括住，使其成为字符串模板的一部分。
 }
 
-fun test2(){
+fun test2() {
     //借助 map() 函数，您可以将一个集合转换为元素数量相同的新集合。
-    val fullMenu  = cookies.map{
+    val fullMenu = cookies.map {
         "${it.name} - $${it.price}"
     }
     fullMenu.forEach {
@@ -80,14 +81,49 @@ fun test2(){
     }
 }
 
-fun test3(){
+fun test3() {
     //借助 filter() 函数，您可以创建集合的子集
     //添加一个布尔表达式，以检查饼干的 softBaked 属性是否等于 true
-    val softBakedMenu  = cookies.filter{
-       it.softBaked
+    val softBakedMenu = cookies.filter {
+        it.softBaked
     }
     println("Soft cookies:")
     softBakedMenu.forEach {
         println("${it.name} - $${it.price}")
     }
+}
+
+fun test4() {
+    //groupBy() 函数可用于根据函数将列表转换为映射。
+
+    //传入一个返回 it.softBaked 的 lambda 表达式。返回类型将为 Map<Boolean, List<Cookie>>。
+
+    val groupedMenu = cookies.groupBy { it.softBaked }
+    val softBakedMenu = groupedMenu[true] ?: listOf()
+    var crunchyMenu = groupedMenu[false] ?: listOf()
+
+    println("Soft cookies:")
+    softBakedMenu.forEach {
+        println("${it.name} - $${it.price}")
+    }
+
+    println("Crunchy cookies:")
+    crunchyMenu.forEach {
+        println("${it.name} - $${it.price}")
+    }
+}
+
+fun test5() {
+    //fold() 函数用于从集合中生成单个值。
+
+    val totalPrice = cookies.fold(0.0) { total, cookie ->
+        total + cookie.price
+    }
+
+    println("Total price: $${totalPrice}")
+}
+
+fun test6(){
+    //sortedBy() 您可以指定一个 lambda 以返回作为排序依据的属性。
+    val alphabeticalMenu = cookies.sortedBy {}
 }
