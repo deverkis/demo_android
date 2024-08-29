6d67eeb570348a91b3207e3b121017a5ffb74a78
fengjianming <fjm20016@qq.com>
2024-08-29 13:39:54 +0800

    测试用例

---
+++ codelabs1/app/src/test/java/cn/kt66/codelabs1/ui/GameViewModelTest.kt
@@ -0,0 +1,12 @@
+package cn.kt66.codelabs1.ui
+
+import org.junit.Test
+
+class GameViewModelTest {
+    private val viewModel = GameViewModel()
+
+    @Test
+    fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset() {
+
+    }
+}
